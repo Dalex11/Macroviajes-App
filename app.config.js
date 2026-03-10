ea14fc5e2a394a24f3b3147ceb58a01c9d543f78
Dalex11 <54454160+Dalex11@users.noreply.github.com>
2026-03-10 16:40:05 -0500

    .env agregado

---
+++ app.config.js
@@ -0,0 +1,74 @@
+import 'dotenv/config';
+
+export default {
+  "expo": {
+    "name": "macroviajes-app",
+    "slug": "macroviajes-app",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/images/icon.png",
+    "scheme": "macroviajesapp",
+    "userInterfaceStyle": "automatic",
+    "newArchEnabled": true,
+    "splash": {
+      "image": "./assets/images/splash-icon.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
+    "ios": {
+      "supportsTablet": true
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/images/adaptive-icon.png",
+        "backgroundColor": "#ffffff"
+      },
+      "edgeToEdgeEnabled": true,
+      "predictiveBackGestureEnabled": false,
+      "permissions": [
+        "android.permission.READ_EXTERNAL_STORAGE",
+        "android.permission.WRITE_EXTERNAL_STORAGE",
+        "android.permission.RECORD_AUDIO",
+        "android.permission.READ_MEDIA_VISUAL_USER_SELECTED",
+        "android.permission.ACCESS_MEDIA_LOCATION",
+        "android.permission.READ_MEDIA_IMAGES",
+        "android.permission.READ_MEDIA_VIDEO",
+        "android.permission.READ_MEDIA_AUDIO",
+        "android.permission.READ_EXTERNAL_STORAGE",
+        "android.permission.WRITE_EXTERNAL_STORAGE",
+        "android.permission.READ_MEDIA_VISUAL_USER_SELECTED",
+        "android.permission.ACCESS_MEDIA_LOCATION",
+        "android.permission.READ_MEDIA_IMAGES",
+        "android.permission.READ_MEDIA_VIDEO",
+        "android.permission.READ_MEDIA_AUDIO"
+      ],
+      "package": "com.anonymous.macroviajesapp"
+    },
+    "web": {
+      "favicon": "./assets/images/favicon.png"
+    },
+    "plugins": [
+      "expo-router",
+      [
+        "expo-media-library",
+        {
+          "photosPermission": "Permite a Macroviajes acceder a tus fotos para guardar promociones.",
+          "savePhotosPermission": "Permite a Macroviajes guardar promociones en tu galería.",
+          "isAccessMediaLocationEnabled": true
+        }
+      ]
+    ],
+    "experiments": {
+      "typedRoutes": true,
+      "reactCompiler": true
+    },
+    "extra": {
+      "firebaseApiKey": process.env.FIREBASE_API_KEY,
+      "firebaseAuthDomain": process.env.FIREBASE_AUTH_DOMAIN,
+      "firebaseProjectId": process.env.FIREBASE_PROJECT_ID,
+      "firebaseStorageBucket": process.env.FIREBASE_STORAGE_BUCKET,
+      "firebaseMessagingSenderId": process.env.FIREBASE_MESSAGING_SENDER_ID,
+      "firebaseAppId": process.env.FIREBASE_APP_ID
+    }
+  }
+};
